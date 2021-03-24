acfd024222d3c3cedb971dc0e7f4d21bc0bf1770
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2021-03-24 13:56:34 -0400

    email config play around

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,19 +1,19 @@
 Rails.application.configure do
 
   # mailer settings
+  Rails.application.routes.default_url_options[:host] = 'https://newportmarine.app'
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'tech_support@newportmarine.app'}
   config.action_mailer.delivery_method = :smtp
-  host = 'hnewportmarine.app'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => 'apikey',
-    :password       => ENV['SENDGRID_API_KEY'],
-    :enable_starttls_auto => true
-  }
-    .
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'https://newportmarine.app',
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY'],
+    authentication: 'plain',
+    enable_starttls_auto: true  }
   
   # Settings specified here will take precedence over those in config/application.rb.
 
